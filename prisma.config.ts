490ee68057fb54fa86ffdb723b8ccdccf8f34507
wenzeljoao <jvitorwenzel@gmail.com>
2026-05-06 21:37:31 -0300

    prism configuration and layer separation

--- prisma.config.ts
+++ prisma.config.ts
@@ -9,6 +9,6 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env["DATABASE_URL"],
+    url: 'postgresql://postgres:senai@localhost:5432/taskflow-api?schema=public',
   },
 });
