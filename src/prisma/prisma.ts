5201f7426e7f3d6251b9f7914fd8888156afc64e
wenzeljoao <jvitorwenzel@gmail.com>
2026-05-21 19:53:04 -0300

    Correção json

--- src/prisma/prisma.ts
+++ src/prisma/prisma.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from "./generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-const connectionString = `postgresql://postgres:senai@localhost:5432/clinic?schema=public`;
+const connectionString = `postgresql://postgres:senai@localhost:5432/taskflow-api?schema=public`;
 
 const adapter = new PrismaPg({ connectionString });
 export const prisma = new PrismaClient({ adapter, log: ['query'] });
